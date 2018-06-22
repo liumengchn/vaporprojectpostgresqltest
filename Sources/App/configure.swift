f97a3fbce7f888bca6eac44ded047438665186ed
liumengchen <liumc1993520@live.com>
2018-06-22 14:25:35 +0800

    Use PostgreSQL as the database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,9 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
+
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    /// Register providers first
-    try services.register(FluentSQLiteProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -17,17 +16,60 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    /*************************************** FluentSQLite ************************************/
+    //    /// Register providers first 1.
+    //    try services.register(FluentSQLiteProvider())
+    //
+    //    // Configure a SQLite database 2.
+    ////    let sqlite = try SQLiteDatabase(storage: .memory)
+    //    let sqlite = try SQLiteDatabase(storage: .file(path: "/Users/liumengchen/Documents/vaporProject/sqlite_dir/db.sqlite"))
+    //
+    //    /// Register the configured SQLite database to the database config.
+    //    var databases = DatabasesConfig()
+    //    databases.add(database: sqlite, as: .sqlite)
+    //    services.register(databases)
+    //
+//        /// Configure migrations 3.
+//        var migrations = MigrationConfig()
+//        migrations.add(model: Acronym.self, database: .sqlite)
+//        services.register(migrations)
 
-    /// Register the configured SQLite database to the database config.
+    /*************************************** FluentPostgreSQL ************************************/
+    /// Register providers first 1.
+    try services.register(FluentPostgreSQLProvider())
+    
+    // Configure a SQLite database 2.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    
+    //3. 本地数据库启用
+//    let databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
+//                                                  port:     5432,
+//                                                  username: "vapor",
+//                                                  database: "vapor",
+//                                                  password: "password")
+//    let database = PostgreSQLDatabase(config: databaseConfig)
+//    databases.add(database: database, as: .psql)
+//    services.register(databases)
+    
+    //4. vapor cloud数据库启用 git commit -am "Use PostgreSQL as the database"  git push
+    let host_name = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let user_name = Environment.get("DATABASE_USER") ?? "vapor"
+    let database_Name = Environment.get("DATABASE_DB") ?? "vapor"
+    let user_password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    
+    let database_config = PostgreSQLDatabaseConfig(hostname: host_name,
+                                                   port: 5432,
+                                                   username: user_name,
+                                                   database: database_Name,
+                                                   password: user_password
+    )
+    let database = PostgreSQLDatabase(config: database_config)
+    databases.add(database: database, as: .psql)
     services.register(databases)
-
-    /// Configure migrations
+    
+    //4.
+    /// Configure migrations 3.
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Acronym.self, database: .psql)
     services.register(migrations)
-
 }
